551bcc8a4ea0951eb0d41969fbe0b65296567447
Vikesh <vikeshkapoor100@gmail.com>
2026-03-17 11:41:34 +0530

    feat: mongo db connection setup

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDb } from './config/connection.js';
 
 dotenv.config();
 const PORT=process.env.PORT;
+const DB_URL=process.env.DB_URL;
 const app=express();
 
 app.use(express.json());
-
+await connectDb(DB_URL);
 app.get('/',(req,res)=>{
     res.send("working");
 })
---
+++ backend/config/connection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDb=async(url)=>{
+    try{
+        await mongoose.connect(url);
+        console.log("Db connected");
+    }catch(err){
+        console.log("DB connection error ,err-",err);
+    }
+}
