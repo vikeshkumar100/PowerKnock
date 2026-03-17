dc132d4e3cdfd8c62a7385b69864ea586bc5fa2f
Vikesh <vikeshkapoor100@gmail.com>
2026-03-17 11:24:03 +0530

    intial commit - folder structure done

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT=process.env.PORT;
+const app=express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("working");
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running on port - ",PORT);
+})
